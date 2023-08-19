4ff42363a1601d1bcf9094a68226d7784bc6ac2d
Unknown1407 <118428191+Unknown1407@users.noreply.github.com>
2023-08-19 12:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
+	let ans = 0;
+	while (num>0) {  // 7 => 3
+		let a = num%2; // 1
+		ans = (ans * 10 ) + a;  // (0 * 10) + 1
+		num = a;
+	}
+	return decimalToBinary;
   
 }
 
