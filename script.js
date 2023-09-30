8e0ab4fb4167e4d816013b741b0f851025685c8f
Biswadeep Dutta <118428191+Unknown1407@users.noreply.github.com>
2023-09-30 15:20:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-function decimalToBinary(num) {
+function decimalToBinary(decimalNumber) {
   //Write you code here
-	let ans = 0;
-	while (num>0) {  
-		let a = num%2; 
-		ans = (ans * 10 ) + a;  
-		num = Math.floor(num/2);
-	}
-	return ans;
+	// let ans = 0;
+	// while (num>0) {  
+	// 	let a = num%2; 
+	// 	if(num%2 == 0)
+	// 	{
+	// 		ans = 1;
+	// 	}
+	// 	else
+	// 	{
+			
+	// 	}
+	// 	ans = (ans * 10 ) + a;  
+	// 	num = Math.floor(num/2);
+	// }
+	return decimalNumber.toString(2);
   
 }
 
-	function Main() {
-		var n = prompt("Enter the decimal number");
-		alert(decimalToBinary(n));
-	}
-	Main();
+	// function Main() {
+	// 	var n = prompt("Enter the decimal number");
+	// 	alert(decimalToBinary(n));
+	// }
+	// Main();
 window.decimalToBinary = decimalToBinary;
